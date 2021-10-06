8e40d770b37af8a4440391175d32d6a153364ed5
玉黍 <docite@gmail.com>
2021-10-06 19:34:15 +0800

    read wid

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import Link from "next/link";
+import { useRouter } from 'next/router'
 import Header from "../components/header";
 
-export default () => (
+export default () => {
+  const router = useRouter()
+  const { wid } = router.query
+  return (
   <main>
     <Header />
+    <section>
+    wid: {wid}
+    </section>
     <section>
       <Link href="/about">
         <a>Go to About Me</a>
       </Link>
     </section>
   </main>
-);
+)};
